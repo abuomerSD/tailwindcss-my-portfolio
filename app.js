e5c2137407c78ed192bb9f193e75af515de6af01
abuomersd <tayeb1293@yahoo.com>
2025-11-26 14:52:16 +0300

    featrue: hero section

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const hamburger = document.getElementById("hamburger");
 const dropdown = document.getElementById("dropdown");
+const closeDropDown = document.getElementById("btn-close");
 
 hamburger.addEventListener("click", (e) => {
+  dropdown.classList.toggle("hidden");
+});
+
+closeDropDown.addEventListener("click", (e) => {
   dropdown.classList.toggle("hidden");
 });
